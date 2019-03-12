49903dd918f134021cc6ddb028b8feff876c107c
Prashant Gupta <prashantgupta@us.ibm.com>
2019-03-12 16:11:37 -0700

    refactoring glide yaml

--- src/activity/activityTracker.go
+++ src/activity/activityTracker.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/activitytracker/src/mouse"
 	"github.com/go-vgo/robotgo"
+	"github.com/prashantgupta24/activity-tracker/src/mouse"
 )
 
 func StartTracker(timeToCheck time.Duration) (activity chan bool, quit chan struct{}) {
